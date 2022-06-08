55473f65a48b40425f5326b9b0b7df28e27163ae
Daniel Queiroz Porto <danielvandalk@gmail.com>
2022-06-08 13:35:57 -0300

    move generic stuff from jump state to air parent state

--- characters/playable/chad/states/air.gd
+++ characters/playable/chad/states/air.gd
@@ -9,8 +9,6 @@ extends "res://characters/playable/chad/states/chad_state.gd"
 
 #--- constants ------------------------------------------------------------------------------------
 
-@export var JUMP_FORCE := -1200
-
 #--- public variables - order: export > normal var > onready --------------------------------------
 
 #--- private variables - order: export > normal var > onready -------------------------------------
@@ -29,28 +27,23 @@ var _gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func enter(msg: = {}) -> void:
 	super(msg)
-	get_parent().enter(msg)
-	_skin.transition_to(_skin.SkinStates.JUMP)
-	if msg.has("velocity"):
-		_character.velocity = msg.velocity
-	
-	_state_machine.set_physics_process(false)
-	await get_tree().process_frame
-	_character.velocity.y = JUMP_FORCE
-	await get_tree().process_frame
-	_state_machine.set_physics_process(true)
-
-
-func unhandled_input(_event: InputEvent) -> void:
-	return
+	_character._disable_collisions()
 
 
 func physics_process(delta: float) -> void:
-	get_parent().physics_process(delta)
+	_character.move_and_slide()
+	_character.velocity.y += _gravity * delta
+	if _character.global_position.y >= _character.ground_level:
+		_character.global_position.y = _character.ground_level
+		if _character.velocity.x != 0:
+			var conserved_velocity = Vector2(_character.velocity.x, 0)
+			_state_machine.transition_to("Ground/Move/Walk", {velocity = conserved_velocity})
+		else:
+			_state_machine.transition_to("Ground/Move/Idle")
 
 
 func exit() -> void:
-	get_parent().exit()
+	_character._enable_collisions()
 	super()
 
 ### -----------------------------------------------------------------------------------------------
