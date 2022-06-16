31fd351669c76e4d371c33d89589cd3dee414a6e
Daniel Queiroz Porto <danielvandalk@gmail.com>
2022-06-15 21:15:29 -0300

    Create generic QuiverAiStateMachine...
    ...and inherit it on evil_chad_ai

--- addons/quiver.beat_em_up/characters/ai/quiver_ai_state_machine.gd
+++ addons/quiver.beat_em_up/characters/ai/quiver_ai_state_machine.gd
@@ -1,7 +1,15 @@
 @tool
+class_name QuiverAiStateMachine
 extends QuiverStateMachine
 
-## Write your doc string for this file here
+## State Machine for AIs
+##
+## It will automatically connect all direct child states [signal QuiverState.state_finished] 
+## signal to the [method _decide_next_action] virtual method. Then the user can extend this class 
+## and override that method with the required logic.
+## [br][br]
+## The Ai State Machine should only take [QuiverAiState] and [QuiverStateSequence] as children.
+
 
 ### Member Variables and Dependencies -------------------------------------------------------------
 #--- signals --------------------------------------------------------------------------------------
@@ -32,6 +40,16 @@ func _ready() -> void:
 		
 		child_state.state_finished.connect(_decide_next_action.bind(child_state.name))
 
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings := PackedStringArray()
+	
+	for child in get_children():
+		if not child is QuiverAiState and not child is QuiverStateSequence:
+			warnings.append("%s is not a QuiverAiState or QuiverSequenceState"%[child.name])
+	
+	return warnings
+
 ### -----------------------------------------------------------------------------------------------
 
 
@@ -42,14 +60,9 @@ func _ready() -> void:
 
 ### Private Methods -------------------------------------------------------------------------------
 
-func _decide_next_action(last_state: StringName) -> void:
-	match last_state:
-		&"ChaseClosestPlayer":
-			transition_to(^"Attack")
-		&"Attack":
-			transition_to(^"Wait")
-		&"Wait":
-			transition_to(^"ChaseClosestPlayer")
+## Virtual method that is executed whenever a state emits the [signal QuiverState.state_finished] signal
+func _decide_next_action(_last_state: StringName) -> void:
+	push_warning("This is a virtual function and should not be used directly, but overriden.")
 
 ### -----------------------------------------------------------------------------------------------
 
