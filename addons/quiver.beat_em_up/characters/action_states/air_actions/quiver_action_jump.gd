50edb98c0fe894a5fe34a8c4b6b44978467b3fbf
Daniel Queiroz Porto <danielvandalk@gmail.com>
2022-07-04 08:46:30 -0300

    Connect ground and air states to placeholder hurt/knockout states

--- addons/quiver.beat_em_up/characters/action_states/air_actions/quiver_action_jump.gd
+++ addons/quiver.beat_em_up/characters/action_states/air_actions/quiver_action_jump.gd
@@ -17,7 +17,9 @@ const AirState = preload(
 #--- public variables - order: export > normal var > onready --------------------------------------
 
 #--- private variables - order: export > normal var > onready -------------------------------------
+
 @export var _path_landing := "Air/Jump/Landing"
+@export var _path_knockout := "Air/Knockout/Launch"
 
 var _air_attack_count := 0
 
@@ -77,5 +79,94 @@ func _handle_landing() -> void:
 	_character.global_position.y = _character.ground_level
 	_character.velocity.y = 0.0
 	_state_machine.transition_to(_path_landing)
+
+
+func _connect_signals() -> void:
+	super()
+	
+	if not _attributes.hurt_requested.is_connected(_on_hurt_requested):
+		_attributes.hurt_requested.connect(_on_hurt_requested)
+	
+	if not _attributes.knockout_requested.is_connected(_on_knockout_requested):
+		_attributes.knockout_requested.connect(_on_knockout_requested)
+
+
+func _disconnect_signals() -> void:
+	super()
+	
+	if _attributes != null:
+		if _attributes.hurt_requested.is_connected(_on_hurt_requested):
+			_attributes.hurt_requested.disconnect(_on_hurt_requested)
+		
+		if _attributes.knockout_requested.is_connected(_on_knockout_requested):
+			_attributes.knockout_requested.disconnect(_on_knockout_requested)
+
+
+func _on_hurt_requested(_hurt_type: QuiverCombatSystem.HurtTypes) -> void:
+	# This is here because ANY hit you receive on air generates a knockout.
+	_state_machine.transition_to(_path_knockout)
+
+
+func _on_knockout_requested() -> void:
+	_state_machine.transition_to(_path_knockout)
+
+### -----------------------------------------------------------------------------------------------
+
+###################################################################################################
+# Custom Inspector ################################################################################
+###################################################################################################
+
+const CUSTOM_PROPERTIES = {
+	"path_knockout": {
+		backing_field = "_path_knockout",
+		type = TYPE_STRING,
+		usage = PROPERTY_USAGE_SCRIPT_VARIABLE,
+		hint = PROPERTY_HINT_NONE,
+		hint_string = QuiverState.HINT_STATE_LIST,
+	},
+#	"": {
+#		backing_field = "",
+#		name = "",
+#		type = TYPE_NIL,
+#		usage = PROPERTY_USAGE_DEFAULT,
+#		hint = PROPERTY_HINT_NONE,
+#		hint_string = "",
+#	},
+}
+
+### Custom Inspector built in functions -----------------------------------------------------------
+
+func _get_property_list() -> Array:
+	var properties: = []
+	
+	for key in CUSTOM_PROPERTIES:
+		var add_property := true
+		var dict: Dictionary = CUSTOM_PROPERTIES[key]
+		if not dict.has("name"):
+			dict.name = key
+		
+		if add_property:
+			properties.append(dict)
+	
+	return properties
+
+
+func _get(property: StringName):
+	var value
+	
+	if property in CUSTOM_PROPERTIES and CUSTOM_PROPERTIES[property].has("backing_field"):
+		value = get(CUSTOM_PROPERTIES[property]["backing_field"])
+	
+	return value
+
+
+func _set(property: StringName, value) -> bool:
+	var has_handled: = false
+	
+	if property in CUSTOM_PROPERTIES and CUSTOM_PROPERTIES[property].has("backing_field"):
+		set(CUSTOM_PROPERTIES[property]["backing_field"], value)
+		has_handled = true
+	
+	return has_handled
 
 ### -----------------------------------------------------------------------------------------------
--- addons/quiver.beat_em_up/characters/action_states/quiver_action_ground.gd
+++ addons/quiver.beat_em_up/characters/action_states/quiver_action_ground.gd
@@ -1,3 +1,4 @@
+@tool
 extends QuiverCharacterState
 
 ## Write your doc string for this file here
@@ -13,10 +14,20 @@ extends QuiverCharacterState
 
 #--- private variables - order: export > normal var > onready -------------------------------------
 
+@export var _path_hurt := "Ground/Hurt"
+@export var _path_knockout := "Air/Knockout/Launch"
+
 ### -----------------------------------------------------------------------------------------------
 
 
 ### Built in Engine Methods -----------------------------------------------------------------------
+
+func _ready() -> void:
+	super()
+	update_configuration_warnings()
+	if Engine.is_editor_hint():
+		QuiverEditorHelper.disable_all_processing(self)
+		return
 
 ### -----------------------------------------------------------------------------------------------
 
@@ -46,5 +57,98 @@ func exit() -> void:
 
 ### Private Methods -------------------------------------------------------------------------------
 
+func _connect_signals() -> void:
+	super()
+	
+	if not _attributes.hurt_requested.is_connected(_on_hurt_requested):
+		_attributes.hurt_requested.connect(_on_hurt_requested)
+	
+	if not _attributes.knockout_requested.is_connected(_on_knockout_requested):
+		_attributes.knockout_requested.connect(_on_knockout_requested)
+
+
+func _disconnect_signals() -> void:
+	super()
+	
+	if _attributes != null:
+		if _attributes.hurt_requested.is_connected(_on_hurt_requested):
+			_attributes.hurt_requested.disconnect(_on_hurt_requested)
+		
+		if _attributes.knockout_requested.is_connected(_on_knockout_requested):
+			_attributes.knockout_requested.disconnect(_on_knockout_requested)
+
+
+func _on_hurt_requested(hurt_type: QuiverCombatSystem.HurtTypes) -> void:
+	_state_machine.transition_to(_path_hurt, {hurt_type = hurt_type})
+
+
+func _on_knockout_requested() -> void:
+	_state_machine.transition_to(_path_knockout)
+
 ### -----------------------------------------------------------------------------------------------
 
+###################################################################################################
+# Custom Inspector ################################################################################
+###################################################################################################
+
+const CUSTOM_PROPERTIES = {
+	"path_hurt": {
+		backing_field = "_path_hurt",
+		type = TYPE_STRING,
+		usage = PROPERTY_USAGE_SCRIPT_VARIABLE,
+		hint = PROPERTY_HINT_NONE,
+		hint_string = QuiverState.HINT_STATE_LIST,
+	},
+	"path_knockout": {
+		backing_field = "_path_knockout",
+		type = TYPE_STRING,
+		usage = PROPERTY_USAGE_SCRIPT_VARIABLE,
+		hint = PROPERTY_HINT_NONE,
+		hint_string = QuiverState.HINT_STATE_LIST,
+	},
+#	"": {
+#		backing_field = "",
+#		name = "",
+#		type = TYPE_NIL,
+#		usage = PROPERTY_USAGE_DEFAULT,
+#		hint = PROPERTY_HINT_NONE,
+#		hint_string = "",
+#	},
+}
+
+### Custom Inspector built in functions -----------------------------------------------------------
+
+func _get_property_list() -> Array:
+	var properties: = []
+	
+	for key in CUSTOM_PROPERTIES:
+		var add_property := true
+		var dict: Dictionary = CUSTOM_PROPERTIES[key]
+		if not dict.has("name"):
+			dict.name = key
+		
+		if add_property:
+			properties.append(dict)
+	
+	return properties
+
+
+func _get(property: StringName):
+	var value
+	
+	if property in CUSTOM_PROPERTIES and CUSTOM_PROPERTIES[property].has("backing_field"):
+		value = get(CUSTOM_PROPERTIES[property]["backing_field"])
+	
+	return value
+
+
+func _set(property: StringName, value) -> bool:
+	var has_handled: = false
+	
+	if property in CUSTOM_PROPERTIES and CUSTOM_PROPERTIES[property].has("backing_field"):
+		set(CUSTOM_PROPERTIES[property]["backing_field"], value)
+		has_handled = true
+	
+	return has_handled
+
+### -----------------------------------------------------------------------------------------------
