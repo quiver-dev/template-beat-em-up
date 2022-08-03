ae067d2baf2377a10caa67e197c7b166efea896c
Daniel Queiroz Porto <danielvandalk@gmail.com>
2022-08-03 09:22:23 -0300

    Fix debug logger performance

--- addons/quiver.beat_em_up/utilities/helpers/quiver_debug_logger.gd
+++ addons/quiver.beat_em_up/utilities/helpers/quiver_debug_logger.gd
@@ -17,12 +17,25 @@ const RELEASE_LOG_FILE = "res://debug_log/%s_session.csv"
 
 #--- private variables - order: export > normal var > onready -------------------------------------
 
+var _current_log := PackedStringArray()
 var _current_log_file := ""
 
 ### -----------------------------------------------------------------------------------------------
 
 
 ### Built in Engine Methods -----------------------------------------------------------------------
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_PREDELETE and not _current_log.is_empty():
+		var dir := Directory.new()
+		if not dir.dir_exists("user://debug_log/"):
+			dir.make_dir_recursive("user://debug_log/")
+		
+		# Improve this later to make it properly with all the error checks
+		var file = File.new()
+		file.open(_current_log_file, File.WRITE)
+		file.store_string("\n".join(_current_log))
+		file.close()
 
 ### -----------------------------------------------------------------------------------------------
 
@@ -42,16 +55,6 @@ func start_new_log() -> void:
 	var file_path = LOG_FILE if OS.has_feature("debug") else RELEASE_LOG_FILE
 	var date_time := Time.get_datetime_string_from_system().replace(":", "-")
 	_current_log_file = file_path%[date_time]
-	
-	var dir := Directory.new()
-	if not dir.dir_exists("user://debug_log/"):
-		dir.make_dir_recursive("user://debug_log/")
-	
-	# Improve this later to make it properly with all the error checks
-	var file = File.new()
-	file.open(_current_log_file, File.WRITE)
-	file.store_string("")
-	file.close()
 
 
 func log_message(msg: PackedStringArray) -> void:
@@ -59,17 +62,8 @@ func log_message(msg: PackedStringArray) -> void:
 		return
 	
 	var date_time := Time.get_datetime_string_from_system()
-	var log_entry: = "%s,%09d,%s \n"%[date_time, Time.get_ticks_msec(), ",".join(msg)]
-	
-	# Improve this later to make it properly with all the error checks
-	var file = File.new()
-	file.open(_current_log_file, File.READ_WRITE)
-	
-	var contents: String = file.get_as_text()
-	contents += log_entry
-	file.store_string(contents)
-	
-	file.close()
+	var log_entry: = "%s,%09d,%s"%[date_time, Time.get_ticks_msec(), ",".join(msg)]
+	_current_log.append(log_entry)
 
 ### -----------------------------------------------------------------------------------------------
 
@@ -77,12 +71,10 @@ func log_message(msg: PackedStringArray) -> void:
 ### Private Methods -------------------------------------------------------------------------------
 
 func _is_logging_enabled() -> bool:
-	return ProjectSettings.get_setting(QuiverCyclicHelper.SETTINGS_LOGGING)
+	return ProjectSettings.get_setting(QuiverCyclicHelper.SETTINGS_LOGGING) and OS.is_debug_build()
 
 
 func _has_created_log_file() -> bool:
-	var file: = File.new()
-	var value := not _current_log_file.is_empty() and file.file_exists(_current_log_file)
-	return value
+	return not _current_log_file.is_empty()
 
 ### -----------------------------------------------------------------------------------------------
