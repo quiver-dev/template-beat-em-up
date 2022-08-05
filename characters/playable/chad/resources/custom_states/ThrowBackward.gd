c69cceb51a7abd9f6d74535a04a987d4bc91b553
Daniel Queiroz Porto <danielvandalk@gmail.com>
2022-08-05 07:34:32 -0300

    Make it possible to alternate backward throws
    One is faster and the other is following noahs timings, it's easier to
    have both in game and feel how they work in game to choose one
    or tweak to something in the middle

--- characters/playable/chad/resources/custom_states/ThrowBackward.gd
+++ characters/playable/chad/resources/custom_states/ThrowBackward.gd
@@ -19,6 +19,9 @@ const GrabState = preload(
 
 #--- private variables - order: export > normal var > onready -------------------------------------
 
+var _skin_state_alt: StringName
+var _skin_state_backup: StringName
+
 @onready var _grab_state := get_parent() as GrabState
 
 ### -----------------------------------------------------------------------------------------------
@@ -32,6 +35,8 @@ func _ready() -> void:
 	if Engine.is_editor_hint():
 		QuiverEditorHelper.disable_all_processing(self)
 		return
+	
+	Events.debug_throw_change_requested.connect(_on_Events_debug_throw_change_requested)
 
 
 func _get_configuration_warnings() -> PackedStringArray:
@@ -84,5 +89,84 @@ func _on_skin_suplex_landed() -> void:
 			_character.global_position.y
 	)
 	_grab_state.grab_target_node.rotation = 0
+
+
+func _on_Events_debug_throw_change_requested(is_slow: bool) -> void:
+	if _skin_state_backup == "":
+		_skin_state_backup = _skin_state
+	
+	if is_slow:
+		_skin_state = _skin_state_alt
+	else:
+		_skin_state = _skin_state_backup
+
+### -----------------------------------------------------------------------------------------------
+
+###################################################################################################
+# Custom Inspector ################################################################################
+###################################################################################################
+
+const CUSTOM_PROPERTIES_LOCAL = {
+	"Attack State":{
+		type = TYPE_NIL,
+		usage = PROPERTY_USAGE_CATEGORY,
+		hint = PROPERTY_HINT_NONE,
+	},
+	"alt_skin_state": {
+		backing_field = "_skin_state_alt",
+		type = TYPE_INT,
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+		hint = PROPERTY_HINT_ENUM,
+		hint_string = \
+				'ExternalEnum{"property": "_skin", "property_name": "_animation_list"}'
+	},
+#	"": {
+#		backing_field = "",
+#		name = "",
+#		type = TYPE_NIL,
+#		usage = PROPERTY_USAGE_DEFAULT,
+#		hint = PROPERTY_HINT_NONE,
+#		hint_string = "",
+#	},
+}
+
+### Custom Inspector built in functions -----------------------------------------------------------
+
+func _get_property_list() -> Array:
+	var properties: = []
+	
+	for key in CUSTOM_PROPERTIES_LOCAL:
+		var add_property := true
+		var dict: Dictionary = CUSTOM_PROPERTIES_LOCAL[key]
+		if not dict.has("name"):
+			dict.name = key
+		
+		match key:
+			"path_combo_state":
+				add_property = _can_combo
+		
+		if add_property:
+			properties.append(dict)
+	
+	return properties
+
+
+func _get(property: StringName):
+	var value
+	
+	if property in CUSTOM_PROPERTIES_LOCAL and CUSTOM_PROPERTIES_LOCAL[property].has("backing_field"):
+		value = get(CUSTOM_PROPERTIES_LOCAL[property]["backing_field"])
+	
+	return value
+
+
+func _set(property: StringName, value) -> bool:
+	var has_handled: = false
+	
+	if property in CUSTOM_PROPERTIES_LOCAL and CUSTOM_PROPERTIES_LOCAL[property].has("backing_field"):
+		set(CUSTOM_PROPERTIES_LOCAL[property]["backing_field"], value)
+		has_handled = true
+	
+	return has_handled
 
 ### -----------------------------------------------------------------------------------------------
