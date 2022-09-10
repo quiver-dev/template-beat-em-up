369d19fd38436f8d211250dcc0ebcf86635dbe64
Daniel Queiroz Porto <danielvandalk@gmail.com>
2022-09-10 15:59:39 -0300

    make debug logger keep only a maximum number of logs
    currently set to 5

--- addons/quiver.beat_em_up/utilities/helpers/static/quiver_debug_logger.gd
+++ addons/quiver.beat_em_up/utilities/helpers/static/quiver_debug_logger.gd
@@ -10,10 +10,13 @@ extends Resource
 
 #--- constants ------------------------------------------------------------------------------------
 
-const LOG_FILE = "user://debug_log/%s_debug_session.csv"
-const RELEASE_LOG_FILE = "res://debug_log/%s_session.csv"
+const LOG_FOLDER = "user://debug_log/"
+const LOG_FILE = LOG_FOLDER+"%s_debug_session.csv"
+const RELEASE_LOG_FILE = LOG_FOLDER+"%s_session.csv"
 
 #--- public variables - order: export > normal var > onready --------------------------------------
+
+@export_range(0,1,1,"or_greater") var max_logs := 5
 
 #--- private variables - order: export > normal var > onready -------------------------------------
 
@@ -24,6 +27,10 @@ var _current_log_file := ""
 
 
 ### Built in Engine Methods -----------------------------------------------------------------------
+
+func _init() -> void:
+	_clear_old_logs()
+
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_PREDELETE and not _current_log.is_empty():
@@ -69,6 +76,22 @@ func log_message(msg: PackedStringArray) -> void:
 
 
 ### Private Methods -------------------------------------------------------------------------------
+
+func _clear_old_logs() -> void:
+	var dir := Directory.new()
+	var files := PackedStringArray()
+	if dir.dir_exists("user://debug_log/"):
+		var error := dir.open("user://debug_log/")
+		if error == OK:
+			files = dir.get_files()
+			
+			if files.size() > max_logs:
+				for index in range(files.size()-max_logs):
+					var file_path := files[index]
+					dir.remove(file_path)
+		else:
+			push_error("Could not open user://debug_log/ Error Code: %s"%[error])
+
 
 func _is_logging_enabled() -> bool:
 	return ProjectSettings.get_setting(QuiverCyclicHelper.SETTINGS_LOGGING) and OS.is_debug_build()
