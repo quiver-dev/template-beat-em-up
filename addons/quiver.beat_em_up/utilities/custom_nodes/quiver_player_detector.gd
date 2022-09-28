2e434b39a9f31e4d26a2224fe918d609d4a8a1b2
Daniel Queiroz Porto <danielvandalk@gmail.com>
2022-09-27 22:28:14 -0300

    Implement quiver_player_detector

---
+++ addons/quiver.beat_em_up/utilities/custom_nodes/quiver_player_detector.gd
@@ -0,0 +1,64 @@
+@tool
+class_name QuiverPlayerDetector
+extends Area2D
+
+## Write your doc string for this file here
+
+### Member Variables and Dependencies -------------------------------------------------------------
+#--- signals --------------------------------------------------------------------------------------
+
+signal player_detected
+
+#--- enums ----------------------------------------------------------------------------------------
+
+#--- constants ------------------------------------------------------------------------------------
+
+#--- public variables - order: export > normal var > onready --------------------------------------
+
+## If `true` player detector will remove itself once it detects a player.
+@export var is_one_shot := true
+
+@export_node_path var path_fight_room := NodePath()
+@export var paths_enemy_spawners: Array[NodePath] = []
+
+@onready var fight_room: QuiverFightRoom = get_node(path_fight_room) \
+		if not path_fight_room.is_empty() else null
+
+#--- private variables - order: export > normal var > onready -------------------------------------
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Built in Engine Methods -----------------------------------------------------------------------
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		QuiverEditorHelper.disable_all_processing(self)
+		QuiverCollisionTypes.apply_preset_to(QuiverCollisionTypes.PRESETS.player_detector, self)
+		return
+	
+	if is_instance_valid(fight_room):
+		QuiverEditorHelper.connect_between(player_detected, fight_room.setup_fight_room)
+	
+	for path in paths_enemy_spawners:
+		var spawner := get_node(path) as QuiverEnemySpawner
+		QuiverEditorHelper.connect_between(player_detected, spawner.spawn_current_wave)
+	
+	QuiverEditorHelper.connect_between(body_entered, _on_body_entered)
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Public Methods --------------------------------------------------------------------------------
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Private Methods -------------------------------------------------------------------------------
+
+func _on_body_entered(body: QuiverCharacter) -> void:
+	if body != null and body.is_in_group("players"):
+		call_deferred("emit_signal", "player_detected")
+		queue_free()
+
+### -----------------------------------------------------------------------------------------------
