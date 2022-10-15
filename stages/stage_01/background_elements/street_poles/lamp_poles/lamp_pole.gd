59e522589be485f1565129b53585e7e7d946435a
Daniel Queiroz Porto <danielvandalk@gmail.com>
2022-10-14 21:30:04 -0300

    Implement Street Lamp Pole assets and logic

---
+++ stages/stage_01/background_elements/street_poles/lamp_poles/lamp_pole.gd
@@ -0,0 +1,53 @@
+@tool
+extends Sprite2D
+
+## Write your doc string for this file here
+
+### Member Variables and Dependencies -------------------------------------------------------------
+#--- signals --------------------------------------------------------------------------------------
+
+#--- enums ----------------------------------------------------------------------------------------
+
+#--- constants ------------------------------------------------------------------------------------
+
+#--- public variables - order: export > normal var > onready --------------------------------------
+
+#--- private variables - order: export > normal var > onready -------------------------------------
+
+var _day_cycle_data := preload("res://stages/_base/day_cycle_data.tres")
+
+@onready var _light := $LampLight as PointLight2D
+@onready var _light_sprite := $LightSprite as Sprite2D
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Built in Engine Methods -----------------------------------------------------------------------
+
+func _ready() -> void:
+	_on_sunset_changed()
+	_day_cycle_data.sunset_changed.connect(_on_sunset_changed)
+	pass
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Public Methods --------------------------------------------------------------------------------
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Private Methods -------------------------------------------------------------------------------
+
+func _on_sunset_changed() -> void:
+	_light.enabled = _day_cycle_data.sunset_transition >= 0.5
+	_light_sprite.visible = _light.enabled
+	if _light.enabled:
+		var progress := smoothstep(0.5, 1.0, _day_cycle_data.sunset_transition)
+		_light.energy = progress
+		_light_sprite.modulate.a = progress
+	elif _light.energy != 0:
+		_light.energy = 0
+		_light_sprite.modulate.a = 0
+
+### -----------------------------------------------------------------------------------------------
