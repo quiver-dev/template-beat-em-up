a78137c2ad78830ce6ef16f32ef0ec771f7f4d6b
Daniel Queiroz Porto <danielvandalk@gmail.com>
2023-02-10 13:07:56 -0300

    add configuration warning for wall hit box

--- addons/quiver.beat_em_up/combat/collision_areas/quiver_wall_hit_box.gd
+++ addons/quiver.beat_em_up/combat/collision_areas/quiver_wall_hit_box.gd
@@ -22,6 +22,22 @@ extends QuiverHitBox
 
 func _ready() -> void:
 	super()
+	attack_data.changed.connect(update_configuration_warnings)
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	const ERROR_KNOCKBACK = "WallHitBox have their own rules for knockback, and attack data's" \
+			+ " knockback properties will be ignored"
+	
+	var warnings := PackedStringArray()
+	
+	if (
+			attack_data.knockback != CombatSystem.KnockbackStrength.NONE
+			or attack_data.launch_angle != 0
+	):
+		warnings.append(ERROR_KNOCKBACK)
+	
+	return warnings
 
 ### -----------------------------------------------------------------------------------------------
 
--- addons/quiver.beat_em_up/combat/quiver_attack_data.gd
+++ addons/quiver.beat_em_up/combat/quiver_attack_data.gd
@@ -1,3 +1,4 @@
+@tool
 class_name QuiverAttackData
 extends Resource
 
@@ -12,14 +13,27 @@ extends Resource
 
 #--- public variables - order: export > normal var > onready --------------------------------------
 
-@export_range(0, 1, 1, "or_greater") var attack_damage = 1
-@export var hurt_type: CombatSystem.HurtTypes = CombatSystem.HurtTypes.HIGH
-@export var knockback: CombatSystem.KnockbackStrength = CombatSystem.KnockbackStrength.NONE
+@export_range(0, 1, 1, "or_greater") var attack_damage = 1:
+	set(value):
+		attack_damage = value
+		emit_changed()
+
+@export var hurt_type: CombatSystem.HurtTypes = CombatSystem.HurtTypes.HIGH:
+	set(value):
+		hurt_type = value
+		emit_changed()
+
+@export var knockback: CombatSystem.KnockbackStrength = CombatSystem.KnockbackStrength.NONE:
+	set(value):
+		knockback = value
+		emit_changed()
+
 @export_range(0, 360, 1) var launch_angle := 0:
 	set(value):
 		launch_angle = value
 		var raw_direction := QuiverMathHelper.get_direction_by_angle(deg_to_rad(launch_angle))
 		launch_vector = raw_direction.reflect(Vector2.RIGHT)
+		emit_changed()
 
 var launch_vector := Vector2.RIGHT
 
